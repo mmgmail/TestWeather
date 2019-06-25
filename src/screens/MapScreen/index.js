7f9315964e936b57e33fe946d8b06ae975f5584e
dngrey <qwerty.qq185@gmail.com>
2019-06-25 18:36:33 +0300

    android map

--- src/screens/MapScreen/index.js
+++ src/screens/MapScreen/index.js
@@ -2,10 +2,12 @@ import React, { PureComponent } from 'react';
 import {
   Text,
   View,
+  StyleSheet
 } from 'react-native';
 import {
   SafeAreaView,
 } from 'react-navigation';
+import MapView from 'react-native-maps';
 
 export default class MapScreen extends PureComponent {
   render() {
@@ -14,10 +16,38 @@ export default class MapScreen extends PureComponent {
 
     return (
       <View style={{ flex: 1 }}>
-        <SafeAreaView forceInset={{ horizontal: 'always' }}>
-          <Text>{banner}</Text>
-        </SafeAreaView>
+        
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        </MapView>
+        
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
