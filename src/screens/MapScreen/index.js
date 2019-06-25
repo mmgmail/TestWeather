a7838c5bdfc2190e2d56907bb32c58b6f5343795
dngrey <qwerty.qq185@gmail.com>
2019-06-25 20:42:32 +0300

    wip marker

--- src/screens/MapScreen/index.js
+++ src/screens/MapScreen/index.js
@@ -2,30 +2,53 @@ import React, { PureComponent } from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Platform
 } from 'react-native';
-import {
-  SafeAreaView,
-} from 'react-navigation';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
+// import console = require('console');
 
 export default class MapScreen extends PureComponent {
+  
+  state = {
+    showMarker: false
+  }
+
+  onHandlerMarkerShow = () => {
+    this.setState({ showMarker: true })
+  }
+
   render() {
-    const { navigation } = this.props;
-    const banner = navigation.getParam('banner');
+    // const { navigation } = this.props;
+
+    const { showMarker } = this.state;
 
     return (
       <View style={{ flex: 1 }}>
-        
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        />
+        {
+          Platform.OS === 'ios' ? null
+          : <MapView
+              style={styles.map}
+              initialRegion={{
+                latitude: 50.4020865,
+                longitude: 30.61468031,
+                latitudeDelta: 1,
+                longitudeDelta: 1,
+              }}
+              onLongPress={this.onHandlerMarkerShow}
+            >
+              {!!showMarker && <Marker
+                onPress={() => {}}
+                coordinate={{
+                  latitude: 50.4020865,
+                  longitude: 30.61468031,
+                }}
+                centerOffset={{ x: -18, y: -60 }}
+                anchor={{ x: 0.69, y: 1 }}
+                pinColor={'tomato'}
+              />}
+            </MapView>
+        }
       </View>
     );
   }
