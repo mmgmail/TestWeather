ee6fbf998d5123cc3d546affc886a535a383ffe6
dngrey <qwerty.qq185@gmail.com>
2019-06-26 07:23:21 +0300

    button

--- src/screens/SearchScreen/index.js
+++ src/screens/SearchScreen/index.js
@@ -2,12 +2,14 @@ import React, { PureComponent } from 'react';
 import {
   Text,
   View,
+  StyleSheet
 } from 'react-native';
 import {
   navigation,
 } from 'react-navigation';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Button } from 'react-native-elements';
 
 export default class MapScreen extends PureComponent {
   
@@ -63,27 +65,50 @@ export default class MapScreen extends PureComponent {
     const banner = navigation.getParam('banner');
     return (
       <View style={{ flex: 1 }}>
-        <GooglePlacesAutocomplete
-          placeholder='Search'
-          minLength={2}
-          autoFocus={false}
-          autoFocus={true}
-          returnKeyType={'search'} 
-          listViewDisplayed={false}
-          fetchDetails={true}
-          onPress={(data, details = null) => {
-              this.onChangeLocation(details.geometry.location);
-            }
-          }
-          query={{
-            key: 'AIzaSyCpTnZOCTTjid_Rej39OI9FsE2_LcMdPg8',
-            language: 'en',
-            types: '(cities)'
-          }}
-          debounce={200}
-        />
-        
+        <View style={{ flex: 1 }}>
+          <View style={styles.searchInput}>
+            <GooglePlacesAutocomplete
+              placeholder='Search'
+              minLength={2}
+              autoFocus={false}
+              autoFocus={true}
+              returnKeyType={'search'} 
+              listViewDisplayed={false}
+              fetchDetails={true}
+              onPress={(data, details = null) => {
+                  this.onChangeLocation(details.geometry.location);
+                }
+              }
+              query={{
+                key: 'AIzaSyCpTnZOCTTjid_Rej39OI9FsE2_LcMdPg8',
+                language: 'en',
+                types: '(cities)'
+              }}
+              debounce={200}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button title={'OK'} />
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row'
+  },
+  searchInput: {
+    position: 'absolute',
+    width: '100%'
+  },
+  button: {
+    width: 50,
+    flexShrink: 0,
+    position: 'absolute',
+    right: 2,
+    top: 2
+  }
+});
